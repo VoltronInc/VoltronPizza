22d2d3c3d5db3d7f775cd2b2ded4205cdf2bfb4f
Yevhen Bohaienko <yevhen@1touch.io>
2020-06-04 00:58:41 +0300

    Example test

--- src/test/java/com/example/pizza/PizzaApplicationTests.java
+++ src/test/java/com/example/pizza/PizzaApplicationTests.java
@@ -1,8 +1,41 @@
 package com.example.pizza;
 
+import com.example.pizza.controller.IngridientController;
+import com.example.pizza.controller.PizzaController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class PizzaApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private IngridientController ingridientController;
+
+	@Autowired
+	private PizzaController pizzaController;
+
+	@Test
+	void contextLoads() {
+		assertThat(ingridientController).isNotNull();
+		assertThat(pizzaController).isNotNull();
+	}
+
+	@Test
+	void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/").contentType(MediaType.TEXT_HTML))
+				.andDo(print())
+				.andExpect(status().is4xxClientError());
+	}
 }
